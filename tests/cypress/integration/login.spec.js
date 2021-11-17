fecb02c95bcb728aa80135f2767a785d86b07314
Iurii Potiekhin <upotekhi@gmail.com>
2021-11-17 17:55:40 +0200

    Cypress installed

---
+++ tests/cypress/integration/login.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Sign in page", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+
+  it("Basic login test", () => {
+    cy.get("[data-cy=email]").type("test@test.com");
+    cy.get("[data-cy=password]").type("0DlX17x1");
+    cy.get("[data-cy=submit]").click();
+
+    cy.get("[data-cy=profile]").should("have.text", " test.user ");
+  });
+});
