c689492fae7d1e63ba1868bb63e47419cc07ed83
Iurii Potiekhin <upotekhi@gmail.com>
2021-11-18 16:02:22 +0200

    WIP

--- tests/cypress/integration/editor.spec.js
+++ tests/cypress/integration/editor.spec.js
@@ -1,4 +1,8 @@
 /// <reference types="cypress" />
+const faker = require("faker");
+const title = faker.lorem.words();
+const description = faker.lorem.sentence();
+const body = faker.lorem.paragraph();
 
 describe("New Article page", () => {
   beforeEach(() => {
@@ -7,6 +11,14 @@ describe("New Article page", () => {
   });
 
   it("Publish Article test", () => {
-    cy.url().should("include", "/editor");
+    cy.get("[data-cy=title]").type(title);
+    cy.get("[data-cy=description]").type(description);
+    cy.get("[data-cy=body]").type(body);
+    cy.get("[data-cy=submit]").click();
+
+    cy.get("h1").should("have.text", title);
+    cy.get("[data-cy=body] > p").should("have.text", body);
+
+    cy.deleteArticle();
   });
 });
--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -5,7 +5,22 @@ Cypress.Commands.add("login", (e, p) => {
       url: "http://localhost:8080/api/users/login",
       body: { user: { email: e, password: p } },
     })
-    .then((response) => {
-      window.localStorage.setItem("jwtToken", response.body.user.token);
+    .then((resp) => {
+      window.localStorage.setItem("jwtToken", resp.body.user.token);
     });
+});
+
+Cypress.Commands.add("deleteArticle", () => {
+  return cy.location("pathname").then((resp) => {
+    const slug = resp.slice(8);
+    const token = window.localStorage.getItem("jwtToken");
+
+    cy.request({
+      method: "DELETE",
+      url: `http://localhost:8080/api/articles${slug}`,
+      headers: {
+        authorization: `Token ${token}`,
+      },
+    });
+  });
 });
